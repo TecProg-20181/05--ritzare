0083203d3e5ec1d80761b8be6ab88b49ff5ccb82
ritzare <joaovictor-fza@hotmail.com>
2018-06-22 19:29:46 -0300

    Add new test

--- diskspace/test_diskspace.py
+++ diskspace/test_diskspace.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import re
 
-from diskspace import bytes_to_readable
+from diskspace import *
 
 class readable(unittest.TestCase):
     def test_type(self):
@@ -13,7 +13,32 @@ class readable(unittest.TestCase):
         bytes_size = bytes_to_readable(0)
         self.assertEqual(bytes_size, '0.00B')
 
+    def test_size2(self):
+        bytes_size = bytes_to_readable(2)
+        self.assertEqual(bytes_size, '1.00Kb')
 
+    def test_size3(self):
+        bytes_size = bytes_to_readable(20000)
+        self.assertEqual(bytes_size, '9.77Mb')
+
+    def test_size4(self):
+        bytes_size = bytes_to_readable(200000000)
+        self.assertEqual(bytes_size, '95.37Gb')    
+
+    def test_size5(self):
+        bytes_size = bytes_to_readable(200000000000)
+        self.assertEqual(bytes_size, '93.13Tb')        
+
+    def test_bytes_not_none(self):
+        bytes = bytes_to_readable(0)
+        self.assertIsNotNone(bytes)
+
+    def test_subprocess(self):
+        command = 'du'
+        result = subprocess_check_output(command)
+        self.assertIsInstance(result, str)
+
+    
 if __name__ == '__main__':
     unittest.main()
         
